90d8f1e62253ded0c8eff8d171d08aa4a1e6abbb
Yurii Zinko <yurazinko@outlook.com>
2016-04-24 18:38:41 +0300

    add volume slider

--- script/main.js
+++ script/main.js
@@ -12,3 +12,50 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 });
+
+/* VOLUME SLIDER */
+
+$(function() {
+
+	var slider = $('#slider');
+	var	tooltip = $('.tooltip');
+
+	tooltip.hide();
+
+	slider.slider({
+		range: "min",
+		min: 1,
+		value: 35,
+
+		start: function(event,ui) {
+		  tooltip.fadeIn('fast');
+		},
+
+		slide: function(event, ui) {
+
+			var value = slider.slider('value'),
+				volume = $('.volume');
+
+			tooltip.css('left', value).text(ui.value);
+
+			if(value <= 5) {
+				volume.css('background-position', '0 0');
+			}
+			else if (value <= 25) {
+				volume.css('background-position', '0 -25px');
+			}
+			else if (value <= 75) {
+				volume.css('background-position', '0 -50px');
+			}
+			else {
+				volume.css('background-position', '0 -75px');
+			};
+
+		},
+
+		stop: function(event,ui) {
+		  tooltip.fadeOut('fast');
+		},
+	});
+
+});
